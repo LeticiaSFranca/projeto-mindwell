9e8db9eafea2f5178a537a3831368b83cc3531e9
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-05-10 09:42:10 -0300

    Colocando link do meu portfólio no rodapé

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import './Footer.style.css';
 export function Footer() {
     return (
         <footer>
-            <p>Desenvolvido por Letícia França <br />Projeto de uma Unidade Curricular da Faculdade</p>
+            <p>Desenvolvido por <a href="https://leticiafranca.com.br" target="_blank" rel="noopener noreferrer">Letícia França</a> <br />Projeto de uma Unidade Curricular da Faculdade</p>
         </footer>
     );
 }
