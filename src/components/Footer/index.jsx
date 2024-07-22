16a1faf3ad75d98cce7b7281d8a446a67458c631
Letícia França <99284224+LeticiaSFranca@users.noreply.github.com>
2024-07-21 23:25:58 -0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,8 +7,8 @@ export function Footer() {
         <footer>
             <p>
                 Copyright &copy; {currentYear}{' '}
-                <a href="https://gfcodes.vercel.app/" target="_blank">
-                    GF Codes
+                <a href="https://spacefordevelopment.vercel.app/" target="_blank">
+                    Space For Development
                 </a>{' '}
                 <br />
                 Projeto de uma Unidade Curricular da Faculdade
